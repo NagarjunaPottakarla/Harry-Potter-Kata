d36c72e3852ffcc3ff2753d64921abd7c8736ce4
Nagarjuna Pottakarla <nagarjuna9189@gmail.com>
2023-05-15 19:54:38 +0530

    Extract one set of four books when items are one copy for first four books

---
+++ src/main/java/Book.java
@@ -0,0 +1,40 @@
+
+public class Book {
+
+	private String title;
+	private int price;
+
+	public Book(String title) {
+		this.title = title;
+		this.price = 8;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public boolean equals(Object o) {
+		if (o == null)
+			return false;
+		if (!(o instanceof Book))
+			return false;
+
+		Book other = (Book) o;
+		return this.title == other.getTitle();
+	}
+	public int hashCode() {
+		return title.hashCode();
+	}
+}
